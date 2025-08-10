2b382aab05f7d2ba72fa37105cbbcf78f490c7f7
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-10 16:02:20 +0600

    function-array-operation

---
+++ function-array-operation.js
@@ -0,0 +1,13 @@
+function sumOfNumber(numbers) {
+    let sum = 0;
+    for (const number of numbers) {
+        console.log(number);
+        sum = sum + number;
+    }
+
+    return sum;
+}
+
+const numbs = [10, 15, 20, 25, 30];
+const sum = sumOfNumber(numbs);
+console.log('Sum of Number is:', sum);
