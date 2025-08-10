f2233109d2e5852b8699400c8ecf1a30f3308b17
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-10 12:05:24 +0600

    function-parameter

---
+++ function-parameter.js
@@ -0,0 +1,44 @@
+function vaatKhao () {
+    console.log('Hand Wash Kore Asho');
+    console.log('Chair a Bosho');
+    console.log('Vaat Nao');
+    console.log('Khao');
+    
+}
+
+// vaatKhao()
+
+function square(number) {
+    const borgo = number * number;
+    console.log(borgo);
+    
+}
+
+square(4);
+square(8);
+square(12);
+square(16);
+square(20);
+
+console.log('----------------------------');
+
+
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+add(6, 70);
+add(105, 23);
+
+console.log('----------------------------');
+
+
+
+function addAll(a,b,c,d,e) {
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+
+addAll(2,4,6,8,10);
