425caeb8bdb206a36d92428a3b2ee47f524d030f
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-12 00:07:48 +0600

    function-task

--- function-task.js
+++ function-task.js
@@ -70,3 +70,20 @@ function countZero (binarayStr) {
 const binaryNum = '1001110001'
 const result = countZero(binaryNum);
 console.log('Total 0 Found in This String:' ,result);
+
+/** Task - 5
+ * Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+ */
+
+function odd_even(numbers) {
+    if (numbers % 2 === 0) {
+        return 'Even Number';
+    }
+
+    else {
+        return 'Odd Number';
+    }
+}
+
+console.log(odd_even(2));
+console.log(odd_even(27));
